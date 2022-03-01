76a318015997440c22abe4e746ce9c9e08d59c18
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2022-03-01 17:57:00 +0100

    Update style.js

--- style.js
+++ style.js
@@ -126,6 +126,8 @@ const ckeStyles = [
   {name: 'Lista desordenada 04', element: 'ul', attributes: {'class': 'bck-ul-4'}},
   {name: 'Lista desordenada 05', element: 'ul', attributes: {'class': 'bck-ul-5'}},
 
+  { name: 'Icono Cerradura', element: 'span', attributes: { 'class': 'icon icon-cerradura' } },
+
   {name: 'Énfasis 01', element: 'span', attributes: {'class': 'bck-enfasis-1'}},
   {name: 'Énfasis 02', element: 'span', attributes: {'class': 'bck-enfasis-2'}},
   {name: 'Énfasis 03', element: 'span', attributes: {'class': 'bck-enfasis-3'}},
